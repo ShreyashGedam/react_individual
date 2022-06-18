ebb40b182372cd5c4bcd96d4c65b3732f71de695
Shreyash Gedam <shreyashakgedam@gmail.com>
2022-06-19 03:40:52 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Allroutes } from "./components/allroutes"
+import { Navbar } from "./components/navbar"
+import { Navbar2 } from "./components/navbar2"
+import {useSelector} from 'react-redux'
+
+function App() {
+
+  
+  
+  return (
+    <div >
+      <Navbar></Navbar>
+      <Navbar2></Navbar2>
+      <Allroutes></Allroutes>     
+    </div>
+  )
+}
+
+export default App
